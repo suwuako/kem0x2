448c7744c2f035bebf2d9e368d03b9b062ef617b
unknown <ronnielow@protonmail.com>
2022-10-20 06:45:59 +0800

    add load_cogs for main

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+import asyncio
+import discord
+
+import src.cogs as cogs
+
 from discord.ext import commands
 
 class main:
     def __init__(self):
         pass
-    
+
+    def load_cogs(self):
+        bot.load_extension("cogs.hello_world")
+
     def run(self):
-        pass
+        self.load_cogs()
+        bot.run(self.token)
 
 if __name__ == "__main__":
+    intents = discord.Intents.all()
+
+    bot = commands.Bot(command_prefix='!', intents=intents)
+    bot.remove_command('help')
+
     main = main()
     main.run()
