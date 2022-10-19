0bd7fda2603e70e5123c1c8ebd2ac14884a08d8a
unknown <ronnielow@protonmail.com>
2022-10-20 07:12:13 +0800

    create secret.json template

--- main.py
+++ main.py
@@ -11,10 +11,20 @@ class main:
         secret = json_methods.read_json(r"json/secret.json")
 
         self.token = secret["token"]
-        self.log_channel = secret["log_channel"]
+        log_channel_id = secret["log_channel"]
         self.alert_list = secret["alert_list"]
 
+        @bot.event
+        async def on_ready():
+            self.log_channel = await bot.fetch_channel(log_channel_id)
 
+            await bot.change_presence(activity=discord.Streaming(name='that ZA with sakamoto',
+                                                                 url='https://www.youtube.com/watch?v=c6VhcpwFZJM'))
+
+            await self.log_channel.send("=== Start Log ===")
+            await self.log_channel.send(f"good morning/afternoon/night")
+
+            print("Online now")
 
     def load_cogs(self):
         bot.load_extension("src.cogs.hello_world")
